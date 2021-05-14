a66a03b4539d923fc162a7e96b0246f4856b9f5d
Himanshu agrawal <himanshuagrawal@Himanshus-Air.bbrouter>
2021-05-14 13:29:34 +0530

    minor changes

--- src/doenv.js
+++ src/doenv.js
@@ -1,5 +1,5 @@
 
 if(process.env.NODE_ENV=='prod')
-   process.env.hostURL='http://3.17.63.250:3000'
+   process.env.hostURL='http://18.221.112.221:3000'
 else
   process.env.hostURL='http://localhost:3000'   
